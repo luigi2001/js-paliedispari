54d27c6336e3f2f61c0a9dafcae97216210bdc30
luigi2001 <luigitanga@virgilio.it>
2021-06-07 19:19:19 +0200

    add esercizio 2 ( parola palindroma) / end

--- js/script.js
+++ js/script.js
@@ -1,44 +1,35 @@
 
 /*-------- pari e dispari-------------- */
 
-var pariDispari = prompt('Pari o dispari?');
+/*var pariDispari = prompt('Pari o dispari?');
 
 
-/* capitalize */
-
-var maiuscola = capitalize(pariDispari);
+var maiuscola = pariDispari.toUpperCase();
 
 
-/* controllo */
-
-if(maiuscola == 'Pari' || maiuscola == 'Dispari'){
+if(maiuscola == 'PARI' || maiuscola == 'DISPARI'){
 
     var numero = parseInt(prompt('Inserisci un numero da 1 a 5:'));
+
     
-
-   /* controllo numero */
-
   if(numero <= 0 || numero > 5){
     alert('Attenzione! Inserisci un numero da 1 a 5');
    } else{
 
-    /* numero random*/
-
-   var numeroRandom = getRndInteger(1,5);
+    var numeroRandom = randomNumber(1,5);
+    
+   
+    var somma = numero + numeroRandom;
+    
    
 
-   /* somma numeri*/
-
-   var somma = numero + numeroRandom;
-   
-
-   var risultato = pariODispari(somma);
+    var risultato = pariODispari(somma);
    
 
    }
 
 } else{
-    alert('Attenzione! devi inserire pari o dispari (inserire tutto in minuscolo)');
+    alert('Attenzione! devi inserire pari o dispari');
 }
 
 
@@ -46,18 +37,40 @@ if(risultato == true){
     document.getElementById('esito').innerHTML = 'COMPLIMENTI! HAI VINTO.';
 }else if(risultato == false){
     document.getElementById('esito').innerHTML = 'MI DISPIACE! HAI PERSO.';
+}*/
+
+
+
+
+/* ------- Palindroma ---------*/
+
+var parola = prompt('inserisci una parola:');
+
+var esito = palindroma(parola);
+
+if(parola < 0 || parola >= 0){
+    alert('Attenzione! Inserire solo parole.')
+}else{
+
+    var risposta = 'la parola inserita non è palindroma';
+
+    if(parola == esito){
+      risposta = 'la parola inserita è palindroma';
+    }
+
+    document.getElementById('esito').innerHTML = risposta;
 }
+
+
 
 
 
 /* funzioni */
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+/* pari e dispari*/
 
-function capitalize(nome){
-    return nome.charAt(0).toUpperCase()+ nome.slice(1);
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function pariODispari(x){
@@ -69,3 +82,15 @@ function pariODispari(x){
     }
 }
 
+/* Palidroma*/
+
+function palindroma(y){
+
+    var reverse = '';
+
+    for (var i = y.length - 1; i >= 0; i--) {
+        reverse += y[i];
+    }
+
+    return reverse;
+}
